7230d23ef588fb39e85c5aab8b407e0b7c911a47
Aadit <mail2aaditsarhadi@gmail.com>
2024-11-07 13:06:24 +0530

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,6 @@
+/* 
+variable = A container that stores value. Behaves as if it were the value it contains.
+Declaration let x;
+Assignment x=100; 
+*/
+Data types in Javascript : Number, string, boolean,null
